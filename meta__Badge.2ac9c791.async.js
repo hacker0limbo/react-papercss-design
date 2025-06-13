31f52f037e0ee9f2bff12e0489c04faa5da5b2f3
hacker0limbo <hacker0limbo@users.noreply.github.com>
2025-06-13 15:53:27 +0000

    deploy: 4828581d8cc287feedc89d2710dea901fcfbc07e

---
+++ meta__Badge.2ac9c791.async.js
@@ -0,0 +1,73 @@
+"use strict";(self.webpackChunkreact_papercss_design=self.webpackChunkreact_papercss_design||[]).push([[2887],{28908:function(u,n,e){var r;e.r(n),e.d(n,{demos:function(){return h}});var g=e(17061),s=e.n(g),v=e(17156),i=e.n(v),t=e(67294),x=e(51690),m=e(71600),h={"src-badge-demo-badgebase":{component:t.memo(t.lazy(function(){return e.e(2433).then(e.bind(e,50619))})),asset:{type:"BLOCK",id:"src-badge-demo-badgebase",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(31338).Z},react:{type:"NPM",value:"18.3.1"},"react-papercss-design":{type:"NPM",value:"0.0.8"}},entry:"index.tsx",title:"\u57FA\u672C",description:"\u4E0D\u540C\u989C\u8272\u7684\u5FBD\u6807"},context:{react:r||(r=e.t(t,2)),"react-papercss-design":m},renderOpts:{compile:function(){var p=i()(s()().mark(function o(){var d,l=arguments;return s()().wrap(function(a){for(;;)switch(a.prev=a.next){case 0:return a.next=2,e.e(7335).then(e.bind(e,37335));case 2:return a.abrupt("return",(d=a.sent).default.apply(d,l));case 3:case"end":return a.stop()}},o)}));function c(){return p.apply(this,arguments)}return c}()}},"src-badge-demo-badgewithparent":{component:t.memo(t.lazy(function(){return e.e(2433).then(e.bind(e,43593))})),asset:{type:"BLOCK",id:"src-badge-demo-badgewithparent",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(57932).Z},react:{type:"NPM",value:"18.3.1"},"react-papercss-design":{type:"NPM",value:"0.0.8"}},entry:"index.tsx",title:"\u5C3A\u5BF8\u5927\u5C0F",description:"\u548C\u7236\u5143\u7D20\u4E00\u8D77\u4F7F\u7528\u65F6\u5927\u5C0F\u81EA\u9002\u5E94"},context:{react:r||(r=e.t(t,2)),"react-papercss-design":m},renderOpts:{compile:function(){var p=i()(s()().mark(function o(){var d,l=arguments;return s()().wrap(function(a){for(;;)switch(a.prev=a.next){case 0:return a.next=2,e.e(7335).then(e.bind(e,37335));case 2:return a.abrupt("return",(d=a.sent).default.apply(d,l));case 3:case"end":return a.stop()}},o)}));function c(){return p.apply(this,arguments)}return c}()}},"src-badge-demo-badgeclosable":{component:t.memo(t.lazy(function(){return e.e(2433).then(e.bind(e,23827))})),asset:{type:"BLOCK",id:"src-badge-demo-badgeclosable",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(29945).Z},react:{type:"NPM",value:"18.3.1"},"react-papercss-design":{type:"NPM",value:"0.0.8"}},entry:"index.tsx",title:"\u53EF\u4EE5\u5220\u9664\u7684\u5FBD\u6807",description:"\u901A\u8FC7closable\u6765\u663E\u793A\u5173\u95ED\u6309\u94AE, onClose\u56DE\u8C03\u51FD\u6570\u52A8\u6001\u63A7\u5236\u5220\u9664"},context:{react:r||(r=e.t(t,2)),"react-papercss-design":m},renderOpts:{compile:function(){var p=i()(s()().mark(function o(){var d,l=arguments;return s()().wrap(function(a){for(;;)switch(a.prev=a.next){case 0:return a.next=2,e.e(7335).then(e.bind(e,37335));case 2:return a.abrupt("return",(d=a.sent).default.apply(d,l));case 3:case"end":return a.stop()}},o)}));function c(){return p.apply(this,arguments)}return c}()}}}},23049:function(u,n,e){e.r(n),e.d(n,{texts:function(){return g}});var r=e(51690);const g=[{value:"\u8FDB\u884C\u6807\u8BB0\u548C\u5206\u7C7B\u7684\u5C0F\u6807\u7B7E\u3002",paraId:0,tocIndex:0},{value:"\u53C2\u8003: ",paraId:1,tocIndex:0},{value:"Badges",paraId:1,tocIndex:0},{value:"\u53C2\u6570",paraId:2,tocIndex:5},{value:"\u8BF4\u660E",paraId:2,tocIndex:5},{value:"\u7C7B\u578B",paraId:2,tocIndex:5},{value:"\u9ED8\u8BA4\u503C",paraId:2,tocIndex:5},{value:"type",paraId:2,tocIndex:5},{value:"\u6837\u5F0F\u7C7B\u578B",paraId:2,tocIndex:5},{value:"'primary'",paraId:2,tocIndex:5},{value:", ",paraId:2,tocIndex:5},{value:"'secondary'",paraId:2,tocIndex:5},{value:", ",paraId:2,tocIndex:5},{value:"'success'",paraId:2,tocIndex:5},{value:", ",paraId:2,tocIndex:5},{value:"'warning'",paraId:2,tocIndex:5},{value:", ",paraId:2,tocIndex:5},{value:"'danger'",paraId:2,tocIndex:5},{value:"-",paraId:2,tocIndex:5},{value:"closable",paraId:2,tocIndex:5},{value:"\u662F\u5426\u663E\u793A\u5173\u95ED\u6309\u94AE",paraId:2,tocIndex:5},{value:"boolean",paraId:2,tocIndex:5},{value:"false",paraId:2,tocIndex:5},{value:"onClose",paraId:2,tocIndex:5},{value:"\u70B9\u51FB\u5173\u95ED\u6309\u94AE\u7684\u56DE\u8C03\u51FD\u6570",paraId:2,tocIndex:5},{value:"(e: MouseEvent) => void",paraId:2,tocIndex:5},{value:"-",paraId:2,tocIndex:5},{value:"closeLabel",paraId:2,tocIndex:5},{value:"\u5173\u95ED\u6309\u94AE\u7684\u6587\u672C",paraId:2,tocIndex:5},{value:"React.ReactNode",paraId:2,tocIndex:5},{value:"'X'",paraId:2,tocIndex:5},{value:"disabled",paraId:2,tocIndex:5},{value:"\u5FBD\u6807\u662F\u5426\u88AB\u7981\u7528",paraId:2,tocIndex:5},{value:"boolean",paraId:2,tocIndex:5},{value:"false",paraId:2,tocIndex:5}]},31338:function(u,n){n.Z=`import React from 'react';
+import { Badge, Space } from 'react-papercss-design';
+
+const App: React.FC = () => {
+  return (
+    <Space wrap>
+      <Badge>Default</Badge>
+      <Badge type="primary">Primary</Badge>
+      <Badge type="secondary">Secondary</Badge>
+      <Badge type="success">Success</Badge>
+      <Badge type="danger">Danger</Badge>
+      <Badge type="warning">Warning</Badge>
+      <Badge disabled>Disabled</Badge>
+    </Space>
+  );
+};
+
+export default App;
+`},29945:function(u,n){n.Z=`import React, { useState } from 'react';
+import { Badge, Space } from 'react-papercss-design';
+
+const App: React.FC = () => {
+  const [tags, setTags] = useState<string[]>(['Unremovable', 'Tag 2', 'Disabled']);
+
+  return (
+    <Space>
+      {tags.map((tag, index) => (
+        <Badge
+          key={tag}
+          closable={index !== 0}
+          disabled={index === tags.length - 1}
+          onClose={() => {
+            setTags(tags.filter((t) => t !== tag));
+          }}
+        >
+          {tag}
+        </Badge>
+      ))}
+    </Space>
+  );
+};
+
+export default App;
+`},57932:function(u,n){n.Z=`import React from 'react';
+import { Badge } from 'react-papercss-design';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>
+        Example h1 heading <Badge>123</Badge>
+      </h1>
+      <h2>
+        Example h2 heading <Badge type="primary">123</Badge>
+      </h2>
+      <h3>
+        Example h3 heading <Badge type="secondary">123</Badge>
+      </h3>
+      <h4>
+        Example h4 heading <Badge type="success">123</Badge>
+      </h4>
+      <h5>
+        Example h5 heading <Badge type="warning">123</Badge>
+      </h5>
+      <h6>
+        Example h6 heading <Badge type="danger">123</Badge>
+      </h6>
+    </>
+  );
+};
+
+export default App;
+`}}]);
