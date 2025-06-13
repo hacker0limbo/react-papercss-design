132fdf002b19a0967e916014428502e6d5258197
hacker0limbo <hacker0limbo@users.noreply.github.com>
2025-06-13 17:04:59 +0000

    deploy: a865cffe59ecb5588119b88965afd349e1e4cb13

---
+++ meta__Article.05fd89c1.async.js
@@ -0,0 +1,30 @@
+"use strict";(self.webpackChunkreact_papercss_design=self.webpackChunkreact_papercss_design||[]).push([[5541],{89462:function(o,a,e){var n;e.r(a),e.d(a,{demos:function(){return l}});var r=e(17061),d=e.n(r),c=e(17156),p=e.n(c),s=e(67294),x=e(71056),u=e(71600),l={"src-article-demo-articlebase":{component:s.memo(s.lazy(function(){return e.e(2433).then(e.bind(e,36653))})),asset:{type:"BLOCK",id:"src-article-demo-articlebase",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(18433).Z},react:{type:"NPM",value:"18.3.1"},"react-papercss-design":{type:"NPM",value:"0.0.11"}},entry:"index.tsx",title:"\u57FA\u672C",description:"\u57FA\u672C\u7684\u6587\u7AE0\u793A\u4F8B"},context:{react:n||(n=e.t(s,2)),"react-papercss-design":u},renderOpts:{compile:function(){var _=p()(d()().mark(function I(){var i,v=arguments;return d()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,e.e(7335).then(e.bind(e,37335));case 2:return t.abrupt("return",(i=t.sent).default.apply(i,v));case 3:case"end":return t.stop()}},I)}));function m(){return _.apply(this,arguments)}return m}()}}}},34976:function(o,a,e){e.r(a),e.d(a,{texts:function(){return r}});var n=e(71056);const r=[{value:"\u914D\u7F6E\u5316\u7684\u5C55\u793A\u4E00\u6BB5\u5185\u5BB9",paraId:0,tocIndex:0},{value:"\u53C2\u8003: ",paraId:1,tocIndex:0},{value:"Articles",paraId:1,tocIndex:0},{value:"\u53C2\u6570",paraId:2,tocIndex:3},{value:"\u8BF4\u660E",paraId:2,tocIndex:3},{value:"\u7C7B\u578B",paraId:2,tocIndex:3},{value:"\u9ED8\u8BA4\u503C",paraId:2,tocIndex:3},{value:"title",paraId:2,tocIndex:3},{value:"\u6587\u7AE0\u6807\u9898",paraId:2,tocIndex:3},{value:"React.ReactNode",paraId:2,tocIndex:3},{value:"-",paraId:2,tocIndex:3},{value:"meta",paraId:2,tocIndex:3},{value:"\u5143\u4FE1\u606F",paraId:2,tocIndex:3},{value:"React.ReactNode",paraId:2,tocIndex:3},{value:"-",paraId:2,tocIndex:3},{value:"textLead",paraId:2,tocIndex:3},{value:"\u5F15\u5BFC\u6587\u672C",paraId:2,tocIndex:3},{value:"React.ReactNode",paraId:2,tocIndex:3},{value:"-",paraId:2,tocIndex:3},{value:"footer",paraId:2,tocIndex:3},{value:"\u5E95\u90E8\u533A\u57DF",paraId:2,tocIndex:3},{value:"React.ReactNode",paraId:2,tocIndex:3},{value:"-",paraId:2,tocIndex:3}]},18433:function(o,a){a.Z=`import React from 'react';
+import { Article, Button, Row } from 'react-papercss-design';
+
+const App: React.FC = () => {
+  return (
+    <Article
+      title="Article Title"
+      meta="Written by Super User on 24 November 2017. Posted in Blog"
+      textLead="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur repellendus excepturi,
+        consequatur illo rerum, non sint asperiores dolore sapiente, vitae blanditiis. Officiis at
+        quaerat modi earum, fugiat magni impedit, aperiam."
+      footer={
+        <Row>
+          <Button>Read More</Button>
+          <Button>5 Comments</Button>
+        </Row>
+      }
+    >
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti iure totam nam debitis
+        aliquid impedit, et quas omnis aspernatur optio molestias ex laborum quia. Ducimus culpa
+        tempore, veritatis officia delectus dolore dignissimos reprehenderit vero, sunt odit placeat
+        est non molestiae ipsa nam velit in iusto hic quasi similique facere. Maxime?
+      </p>
+    </Article>
+  );
+};
+
+export default App;
+`}}]);
