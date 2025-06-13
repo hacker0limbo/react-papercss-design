e287cfde0e0d55b6ea08b2b39b88f3a0a76293ab
hacker0limbo <hacker0limbo@users.noreply.github.com>
2025-06-13 16:20:51 +0000

    deploy: 67d05fc26d3e64bf88a2a3879c351c821d2e64f1

---
+++ meta__Progress.6099a19f.async.js
@@ -0,0 +1,86 @@
+"use strict";(self.webpackChunkreact_papercss_design=self.webpackChunkreact_papercss_design||[]).push([[1540],{65806:function(l,r,e){var a;e.r(r),e.d(r,{demos:function(){return f}});var I=e(17061),p=e.n(I),v=e(17156),i=e.n(v),t=e(67294),P=e(34389),m=e(71600),f={"src-progress-demo-progressbase":{component:t.memo(t.lazy(function(){return e.e(2433).then(e.bind(e,13663))})),asset:{type:"BLOCK",id:"src-progress-demo-progressbase",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(60425).Z},react:{type:"NPM",value:"18.3.1"},"react-papercss-design":{type:"NPM",value:"0.0.10"}},entry:"index.tsx",title:"\u57FA\u672C",description:"\u57FA\u672C\u7684\u8FDB\u5EA6\u6761"},context:{react:a||(a=e.t(t,2)),"react-papercss-design":m},renderOpts:{compile:function(){var c=i()(p()().mark(function d(){var s,u=arguments;return p()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(7335).then(e.bind(e,37335));case 2:return n.abrupt("return",(s=n.sent).default.apply(s,u));case 3:case"end":return n.stop()}},d)}));function o(){return c.apply(this,arguments)}return o}()}},"src-progress-demo-progressstriped":{component:t.memo(t.lazy(function(){return e.e(2433).then(e.bind(e,63920))})),asset:{type:"BLOCK",id:"src-progress-demo-progressstriped",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(77566).Z},react:{type:"NPM",value:"18.3.1"},"react-papercss-design":{type:"NPM",value:"0.0.10"}},entry:"index.tsx",title:"\u6761\u7EB9\u7C7B\u578B",description:"\u901A\u8FC7\u8BBE\u7F6Estriped\u663E\u793A\u4E3A\u6761\u7EB9\u7C7B\u578B\u7684\u8FDB\u5EA6\u6761"},context:{react:a||(a=e.t(t,2)),"react-papercss-design":m},renderOpts:{compile:function(){var c=i()(p()().mark(function d(){var s,u=arguments;return p()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(7335).then(e.bind(e,37335));case 2:return n.abrupt("return",(s=n.sent).default.apply(s,u));case 3:case"end":return n.stop()}},d)}));function o(){return c.apply(this,arguments)}return o}()}},"src-progress-demo-progressinfo":{component:t.memo(t.lazy(function(){return e.e(2433).then(e.bind(e,6983))})),asset:{type:"BLOCK",id:"src-progress-demo-progressinfo",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(34857).Z},react:{type:"NPM",value:"18.3.1"},"react-papercss-design":{type:"NPM",value:"0.0.10"}},entry:"index.tsx",title:"\u663E\u793A\u6570\u503C",description:"\u901A\u8FC7showInfo\u5C5E\u6027\u663E\u793A\u6570\u503C, format\u5C5E\u6027\u81EA\u5B9A\u4E49\u663E\u793A\u7684\u5185\u5BB9"},context:{react:a||(a=e.t(t,2)),"react-papercss-design":m},renderOpts:{compile:function(){var c=i()(p()().mark(function d(){var s,u=arguments;return p()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(7335).then(e.bind(e,37335));case 2:return n.abrupt("return",(s=n.sent).default.apply(s,u));case 3:case"end":return n.stop()}},d)}));function o(){return c.apply(this,arguments)}return o}()}},"src-progress-demo-progressdynamic":{component:t.memo(t.lazy(function(){return e.e(2433).then(e.bind(e,96893))})),asset:{type:"BLOCK",id:"src-progress-demo-progressdynamic",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(18697).Z},react:{type:"NPM",value:"18.3.1"},"react-papercss-design":{type:"NPM",value:"0.0.10"}},entry:"index.tsx",title:"\u52A8\u6001\u5C55\u793A",description:"\u4F1A\u52A8\u7684\u8FDB\u5EA6\u6761\u624D\u662F\u597D\u8FDB\u5EA6\u6761"},context:{react:a||(a=e.t(t,2)),"react-papercss-design":m},renderOpts:{compile:function(){var c=i()(p()().mark(function d(){var s,u=arguments;return p()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(7335).then(e.bind(e,37335));case 2:return n.abrupt("return",(s=n.sent).default.apply(s,u));case 3:case"end":return n.stop()}},d)}));function o(){return c.apply(this,arguments)}return o}()}}}},75103:function(l,r,e){e.r(r),e.d(r,{texts:function(){return I}});var a=e(34389);const I=[{value:"\u5C55\u793A\u64CD\u4F5C\u7684\u5F53\u524D\u8FDB\u5EA6\u3002",paraId:0,tocIndex:0},{value:"\u53C2\u8003: ",paraId:1,tocIndex:0},{value:"Progress",paraId:1,tocIndex:0},{value:"\u53C2\u6570",paraId:2,tocIndex:6},{value:"\u8BF4\u660E",paraId:2,tocIndex:6},{value:"\u7C7B\u578B",paraId:2,tocIndex:6},{value:"\u9ED8\u8BA4\u503C",paraId:2,tocIndex:6},{value:"type",paraId:2,tocIndex:6},{value:"\u8FDB\u5EA6\u6761\u7C7B\u578B",paraId:2,tocIndex:6},{value:"primary",paraId:2,tocIndex:6},{value:", ",paraId:2,tocIndex:6},{value:"secondary",paraId:2,tocIndex:6},{value:", ",paraId:2,tocIndex:6},{value:"success",paraId:2,tocIndex:6},{value:", ",paraId:2,tocIndex:6},{value:"warning",paraId:2,tocIndex:6},{value:", ",paraId:2,tocIndex:6},{value:"danger",paraId:2,tocIndex:6},{value:", ",paraId:2,tocIndex:6},{value:"muted",paraId:2,tocIndex:6},{value:"default",paraId:2,tocIndex:6},{value:"percent",paraId:2,tocIndex:6},{value:"\u8FDB\u5EA6\u6761\u767E\u5206\u6BD4",paraId:2,tocIndex:6},{value:"number",paraId:2,tocIndex:6},{value:"0",paraId:2,tocIndex:6},{value:"showInfo",paraId:2,tocIndex:6},{value:"\u662F\u5426\u663E\u793A\u8FDB\u5EA6\u6570\u503C",paraId:2,tocIndex:6},{value:"boolean",paraId:2,tocIndex:6},{value:"false",paraId:2,tocIndex:6},{value:"format",paraId:2,tocIndex:6},{value:"\u5185\u5BB9\u7684\u6A21\u677F\u51FD\u6570",paraId:2,tocIndex:6},{value:"(percent: number) => string",paraId:2,tocIndex:6},{value:"`(percent: number) => `${percent} %`",paraId:2,tocIndex:6},{value:"striped",paraId:2,tocIndex:6},{value:"\u662F\u5426\u5C55\u793A\u6CE2\u7EB9\u7C7B\u578B",paraId:2,tocIndex:6},{value:"boolean",paraId:2,tocIndex:6},{value:"false",paraId:2,tocIndex:6}]},60425:function(l,r){r.Z=`import React from 'react';
+import { Progress, Space } from 'react-papercss-design';
+
+const App: React.FC = () => {
+  return (
+    <Space direction="vertical">
+      <Progress percent={10} />
+      <Progress percent={25} type="secondary" />
+      <Progress percent={40} type="success" />
+      <Progress percent={55} type="warning" />
+      <Progress percent={75} type="danger" />
+      <Progress percent={100} type="muted" />
+    </Space>
+  );
+};
+
+export default App;
+`},18697:function(l,r){r.Z=`import React, { useState } from 'react';
+import { Button, Progress, Space } from 'react-papercss-design';
+
+const App: React.FC = () => {
+  const [percent, setPercent] = useState(20);
+
+  const increase = () => {
+    setPercent((prevPercent) => {
+      const newPercent = prevPercent + 10;
+      if (newPercent > 100) {
+        return 100;
+      }
+      return newPercent;
+    });
+  };
+
+  const decline = () => {
+    setPercent((prevPercent) => {
+      const newPercent = prevPercent - 10;
+      if (newPercent < 0) {
+        return 0;
+      }
+      return newPercent;
+    });
+  };
+
+  return (
+    <Space direction="vertical">
+      <div>
+        <Button onClick={increase}>+</Button>
+        <Button onClick={decline}>-</Button>
+      </div>
+      <Progress percent={percent} showInfo />
+    </Space>
+  );
+};
+
+export default App;
+`},34857:function(l,r){r.Z=`import React from 'react';
+import { Progress, Space } from 'react-papercss-design';
+
+const App: React.FC = () => {
+  return (
+    <Space direction="vertical">
+      <Progress percent={25} showInfo />
+      <Progress percent={75} showInfo format={(percent) => (percent / 100).toFixed(2)} />
+    </Space>
+  );
+};
+
+export default App;
+`},77566:function(l,r){r.Z=`import React from 'react';
+import { Progress, Space } from 'react-papercss-design';
+
+const App: React.FC = () => {
+  return (
+    <Space direction="vertical">
+      <Progress percent={10} striped />
+      <Progress percent={25} striped type="secondary" />
+      <Progress percent={40} striped type="success" />
+      <Progress percent={55} striped type="warning" />
+      <Progress percent={75} striped type="danger" />
+      <Progress percent={100} striped type="muted" />
+    </Space>
+  );
+};
+
+export default App;
+`}}]);
