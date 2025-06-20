98009bb77e3758813bde1debb291f89043998582
hacker0limbo <hacker0limbo@users.noreply.github.com>
2025-06-20 15:47:12 +0000

    deploy: 532f30ad2274c24e9e01dfbd65f026f1f3b357d6

---
+++ meta__Checkbox.9ead3433.async.js
@@ -0,0 +1,103 @@
+"use strict";(self.webpackChunkreact_papercss_design=self.webpackChunkreact_papercss_design||[]).push([[4059],{93175:function(u,t,e){var c;e.r(t),e.d(t,{demos:function(){return I}});var h=e(17061),o=e.n(h),v=e(17156),i=e.n(v),a=e(67294),C=e(97987),x=e(12276),I={"src-checkbox-demo-checkboxbase":{component:a.memo(a.lazy(function(){return e.e(2433).then(e.bind(e,27366))})),asset:{type:"BLOCK",id:"src-checkbox-demo-checkboxbase",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(47017).Z},react:{type:"NPM",value:"18.3.1"},"react-papercss-design":{type:"NPM",value:"0.1.0"}},entry:"index.tsx",title:"\u57FA\u672C\u4F7F\u7528",description:"\u57FA\u672C\u4F7F\u7528"},context:{react:c||(c=e.t(a,2)),"react-papercss-design":x},renderOpts:{compile:function(){var d=i()(o()().mark(function p(){var r,l=arguments;return o()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(7335).then(e.bind(e,37335));case 2:return n.abrupt("return",(r=n.sent).default.apply(r,l));case 3:case"end":return n.stop()}},p)}));function s(){return d.apply(this,arguments)}return s}()}},"src-checkbox-demo-checkboxdisabled":{component:a.memo(a.lazy(function(){return e.e(2433).then(e.bind(e,64682))})),asset:{type:"BLOCK",id:"src-checkbox-demo-checkboxdisabled",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(53502).Z},react:{type:"NPM",value:"18.3.1"},"react-papercss-design":{type:"NPM",value:"0.1.0"}},entry:"index.tsx",title:"\u7981\u7528",description:"\u7981\u7528\u591A\u9009\u6846"},context:{react:c||(c=e.t(a,2)),"react-papercss-design":x},renderOpts:{compile:function(){var d=i()(o()().mark(function p(){var r,l=arguments;return o()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(7335).then(e.bind(e,37335));case 2:return n.abrupt("return",(r=n.sent).default.apply(r,l));case 3:case"end":return n.stop()}},p)}));function s(){return d.apply(this,arguments)}return s}()}},"src-checkbox-demo-checkboxcontrolled":{component:a.memo(a.lazy(function(){return e.e(2433).then(e.bind(e,51474))})),asset:{type:"BLOCK",id:"src-checkbox-demo-checkboxcontrolled",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(75309).Z},react:{type:"NPM",value:"18.3.1"},"react-papercss-design":{type:"NPM",value:"0.1.0"}},entry:"index.tsx",title:"\u53D7\u63A7",description:"\u4F7F\u7528checked\u548ConChange\u8FDB\u884C\u53D7\u63A7"},context:{react:c||(c=e.t(a,2)),"react-papercss-design":x},renderOpts:{compile:function(){var d=i()(o()().mark(function p(){var r,l=arguments;return o()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(7335).then(e.bind(e,37335));case 2:return n.abrupt("return",(r=n.sent).default.apply(r,l));case 3:case"end":return n.stop()}},p)}));function s(){return d.apply(this,arguments)}return s}()}},"src-checkbox-demo-checkboxgroupbase":{component:a.memo(a.lazy(function(){return e.e(2433).then(e.bind(e,11862))})),asset:{type:"BLOCK",id:"src-checkbox-demo-checkboxgroupbase",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(8686).Z},react:{type:"NPM",value:"18.3.1"},"react-papercss-design":{type:"NPM",value:"0.1.0"}},entry:"index.tsx",title:"\u7EC4\u5408",description:"\u4F7F\u7528Checkbox.Group\u6E32\u67D3\u4E00\u7CFB\u5217\u7684Checkbox"},context:{react:c||(c=e.t(a,2)),"react-papercss-design":x},renderOpts:{compile:function(){var d=i()(o()().mark(function p(){var r,l=arguments;return o()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(7335).then(e.bind(e,37335));case 2:return n.abrupt("return",(r=n.sent).default.apply(r,l));case 3:case"end":return n.stop()}},p)}));function s(){return d.apply(this,arguments)}return s}()}},"src-checkbox-demo-checkboxgroupcontrolled":{component:a.memo(a.lazy(function(){return e.e(2433).then(e.bind(e,16974))})),asset:{type:"BLOCK",id:"src-checkbox-demo-checkboxgroupcontrolled",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(7903).Z},react:{type:"NPM",value:"18.3.1"},"react-papercss-design":{type:"NPM",value:"0.1.0"}},entry:"index.tsx",title:"\u7EC4\u5408\u53D7\u63A7",description:"\u4F7F\u7528value\u548ConChange\u8FDB\u884C\u53D7\u63A7"},context:{react:c||(c=e.t(a,2)),"react-papercss-design":x},renderOpts:{compile:function(){var d=i()(o()().mark(function p(){var r,l=arguments;return o()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(7335).then(e.bind(e,37335));case 2:return n.abrupt("return",(r=n.sent).default.apply(r,l));case 3:case"end":return n.stop()}},p)}));function s(){return d.apply(this,arguments)}return s}()}},"src-checkbox-demo-checkboxgroupconfig":{component:a.memo(a.lazy(function(){return e.e(2433).then(e.bind(e,68016))})),asset:{type:"BLOCK",id:"src-checkbox-demo-checkboxgroupconfig",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(36503).Z},react:{type:"NPM",value:"18.3.1"},"react-papercss-design":{type:"NPM",value:"0.1.0"}},entry:"index.tsx",title:"\u7EC4\u5408\u914D\u7F6E",description:"\u4F7F\u7528options\u8FDB\u884C\u914D\u7F6E\u5316"},context:{react:c||(c=e.t(a,2)),"react-papercss-design":x},renderOpts:{compile:function(){var d=i()(o()().mark(function p(){var r,l=arguments;return o()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(7335).then(e.bind(e,37335));case 2:return n.abrupt("return",(r=n.sent).default.apply(r,l));case 3:case"end":return n.stop()}},p)}));function s(){return d.apply(this,arguments)}return s}()}}}},30074:function(u,t,e){e.r(t),e.d(t,{texts:function(){return h}});var c=e(97987);const h=[{value:"\u7528\u4E8E\u5728\u4E00\u7EC4\u53EF\u9009\u9879\u4E2D\u8FDB\u884C\u591A\u9879\u9009\u62E9",paraId:0,tocIndex:0},{value:"\u7531\u4E8E",paraId:1,tocIndex:8},{value:"Checkbox",paraId:1,tocIndex:8},{value:"\u7EC4\u4EF6\u662F\u5355\u72EC\u4F7F\u7528\u7684, \u4ED6\u548C\u5728",paraId:1,tocIndex:8},{value:".form-group",paraId:1,tocIndex:8},{value:"\u4E0B\u8868\u73B0\u5F62\u5F0F\u4E0D\u540C, \u4ED6\u7684\u5E03\u5C40\u5C5E\u6027\u88AB\u8BBE\u7F6E\u6210\u4E86",paraId:1,tocIndex:8},{value:"display: inline-block",paraId:1,tocIndex:8},{value:"\u800C\u4E0D\u662F",paraId:1,tocIndex:8},{value:"display: block",paraId:1,tocIndex:8},{value:"Checkbox.Group",paraId:2,tocIndex:8},{value:"\u662F\u65B0\u5B9E\u73B0\u7684 API, \u7528\u4E8E\u7F57\u5217\u4E00\u7CFB\u5217\u76F8\u5173\u7684",paraId:2,tocIndex:8},{value:"Checkbox",paraId:2,tocIndex:8},{value:"\u9009\u9879",paraId:2,tocIndex:8},{value:"\u53C2\u6570",paraId:3,tocIndex:10},{value:"\u8BF4\u660E",paraId:3,tocIndex:10},{value:"\u7C7B\u578B",paraId:3,tocIndex:10},{value:"\u9ED8\u8BA4\u503C",paraId:3,tocIndex:10},{value:"checked",paraId:3,tocIndex:10},{value:"\u53D7\u63A7\u4E0B\u591A\u9009\u77FF\u9009\u4E2D",paraId:3,tocIndex:10},{value:"boolean",paraId:3,tocIndex:10},{value:"-",paraId:3,tocIndex:10},{value:"defaultValue",paraId:3,tocIndex:10},{value:"\u975E\u53D7\u63A7\u4E0B\u591A\u9009\u6846\u9009\u4E2D",paraId:3,tocIndex:10},{value:"boolean",paraId:3,tocIndex:10},{value:"-",paraId:3,tocIndex:10},{value:"onChange",paraId:3,tocIndex:10},{value:"\u591A\u9009\u6846\u9009\u4E2D\u53D8\u5316\u65F6\u7684\u56DE\u8C03",paraId:3,tocIndex:10},{value:"(checked: boolean, e: ChangeEvent) => void",paraId:3,tocIndex:10},{value:"-",paraId:3,tocIndex:10},{value:"disabled",paraId:3,tocIndex:10},{value:"\u662F\u5426\u7981\u7528",paraId:3,tocIndex:10},{value:"boolean",paraId:3,tocIndex:10},{value:"-",paraId:3,tocIndex:10},{value:"\u53C2\u6570",paraId:4,tocIndex:11},{value:"\u8BF4\u660E",paraId:4,tocIndex:11},{value:"\u7C7B\u578B",paraId:4,tocIndex:11},{value:"\u9ED8\u8BA4\u503C",paraId:4,tocIndex:11},{value:"value",paraId:4,tocIndex:11},{value:"\u53D7\u63A7\u4E0B\u9009\u4E2D\u67D0\u591A\u9009\u6846\u7684\u503C",paraId:4,tocIndex:11},{value:"(string | number)[]",paraId:4,tocIndex:11},{value:"-",paraId:4,tocIndex:11},{value:"defaultValue",paraId:4,tocIndex:11},{value:"\u975E\u53D7\u63A7\u4E0B\u9009\u4E2D\u67D0\u591A\u9009\u6846\u7684\u503C",paraId:4,tocIndex:11},{value:"(string | number)[]",paraId:4,tocIndex:11},{value:"[]",paraId:4,tocIndex:11},{value:"onChange",paraId:4,tocIndex:11},{value:"\u67D0\u591A\u9009\u6846\u9009\u4E2D\u53D8\u5316\u65F6\u7684\u56DE\u8C03",paraId:4,tocIndex:11},{value:"(value: (string | number)[], e: ChangeEvent) => void",paraId:4,tocIndex:11},{value:"-",paraId:4,tocIndex:11},{value:"disabled",paraId:4,tocIndex:11},{value:"\u6240\u6709\u591A\u9009\u6846\u662F\u5426\u5747\u88AB\u7981\u7528",paraId:4,tocIndex:11},{value:"boolean",paraId:4,tocIndex:11},{value:"-",paraId:4,tocIndex:11},{value:"options",paraId:4,tocIndex:11},{value:"\u914D\u7F6E\u65B9\u5F0F\u5B9E\u73B0",paraId:4,tocIndex:11},{value:"{ label: ReactNode, value: string | number, disabled: boolean }[]",paraId:4,tocIndex:11},{value:"-",paraId:4,tocIndex:11}]},47017:function(u,t){t.Z=`import React from 'react';
+import { Checkbox } from 'react-papercss-design';
+
+const App: React.FC = () => {
+  return <Checkbox>Checkbox</Checkbox>;
+};
+
+export default App;
+`},75309:function(u,t){t.Z=`import React, { useState } from 'react';
+import { Checkbox } from 'react-papercss-design';
+
+const App: React.FC = () => {
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <Checkbox
+      checked={checked}
+      onChange={(c, e) => {
+        setChecked(c);
+        console.log('checkbox onchange value and event:', c, e);
+      }}
+    >
+      Controlled Checkbox
+    </Checkbox>
+  );
+};
+
+export default App;
+`},53502:function(u,t){t.Z=`import React from 'react';
+import { Checkbox } from 'react-papercss-design';
+
+const App: React.FC = () => {
+  return <Checkbox disabled>Disabled Radio</Checkbox>;
+};
+
+export default App;
+`},8686:function(u,t){t.Z=`import React from 'react';
+import { Checkbox } from 'react-papercss-design';
+
+const App: React.FC = () => {
+  return (
+    <Checkbox.Group defaultValue={[1]}>
+      <Checkbox value={1}>Checkbox1</Checkbox>
+      <Checkbox value={2}>Checkbox2</Checkbox>
+      <Checkbox value={3} disabled>
+        Checkbox3
+      </Checkbox>
+    </Checkbox.Group>
+  );
+};
+
+export default App;
+`},36503:function(u,t){t.Z=`import React from 'react';
+import { Checkbox } from 'react-papercss-design';
+
+const App: React.FC = () => {
+  return (
+    <Checkbox.Group
+      options={[
+        {
+          label: 'Checkbox1',
+          value: 1,
+        },
+        {
+          label: 'Checkbox2',
+          value: 2,
+        },
+        {
+          label: 'Checkbox3',
+          value: 3,
+          disabled: true,
+        },
+      ]}
+    />
+  );
+};
+
+export default App;
+`},7903:function(u,t){t.Z=`import React, { useState } from 'react';
+import { Checkbox } from 'react-papercss-design';
+
+const App: React.FC = () => {
+  const [value, setValue] = useState<number[]>([1, 3]);
+
+  return (
+    <Checkbox.Group
+      value={value}
+      onChange={(v, e) => {
+        console.log('checkboxGroup onChange value and event', v, e);
+        setValue(v as number[]);
+      }}
+    >
+      <Checkbox value={1}>Checkbox1</Checkbox>
+      <Checkbox value={2}>Checkbox2</Checkbox>
+      <Checkbox value={3} disabled>
+        Checkbox3
+      </Checkbox>
+    </Checkbox.Group>
+  );
+};
+
+export default App;
+`}}]);
---
+++ meta__Select.845f0ccb.async.js
@@ -0,0 +1,71 @@
+"use strict";(self.webpackChunkreact_papercss_design=self.webpackChunkreact_papercss_design||[]).push([[7601],{87775:function(u,t,e){var s;e.r(t),e.d(t,{demos:function(){return g}});var m=e(17061),l=e.n(m),f=e(17156),i=e.n(f),a=e(67294),O=e(31736),v=e(12276),g={"src-select-demo-selectbase":{component:a.memo(a.lazy(function(){return e.e(2433).then(e.bind(e,21951))})),asset:{type:"BLOCK",id:"src-select-demo-selectbase",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(39887).Z},react:{type:"NPM",value:"18.3.1"},"react-papercss-design":{type:"NPM",value:"0.1.0"}},entry:"index.tsx",title:"\u57FA\u672C\u4F7F\u7528",description:"\u57FA\u672C\u4F7F\u7528, \u9ED8\u8BA4\u9009\u4E2D\u7B2C\u4E00\u4E2A\u9009\u9879"},context:{react:s||(s=e.t(a,2)),"react-papercss-design":v},renderOpts:{compile:function(){var c=i()(l()().mark(function d(){var r,p=arguments;return l()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(7335).then(e.bind(e,37335));case 2:return n.abrupt("return",(r=n.sent).default.apply(r,p));case 3:case"end":return n.stop()}},d)}));function o(){return c.apply(this,arguments)}return o}()}},"src-select-demo-selectcontrolled":{component:a.memo(a.lazy(function(){return e.e(2433).then(e.bind(e,1107))})),asset:{type:"BLOCK",id:"src-select-demo-selectcontrolled",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(89844).Z},react:{type:"NPM",value:"18.3.1"},"react-papercss-design":{type:"NPM",value:"0.1.0"}},entry:"index.tsx",title:"\u53D7\u63A7",description:"\u4F7F\u7528value\u548ConChange\u8FDB\u884C\u53D7\u63A7"},context:{react:s||(s=e.t(a,2)),"react-papercss-design":v},renderOpts:{compile:function(){var c=i()(l()().mark(function d(){var r,p=arguments;return l()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(7335).then(e.bind(e,37335));case 2:return n.abrupt("return",(r=n.sent).default.apply(r,p));case 3:case"end":return n.stop()}},d)}));function o(){return c.apply(this,arguments)}return o}()}},"src-select-demo-selectdisabled":{component:a.memo(a.lazy(function(){return e.e(2433).then(e.bind(e,49057))})),asset:{type:"BLOCK",id:"src-select-demo-selectdisabled",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(91157).Z},react:{type:"NPM",value:"18.3.1"},"react-papercss-design":{type:"NPM",value:"0.1.0"}},entry:"index.tsx",title:"\u7981\u7528",description:"\u7981\u7528\u4E0B\u62C9\u6846"},context:{react:s||(s=e.t(a,2)),"react-papercss-design":v},renderOpts:{compile:function(){var c=i()(l()().mark(function d(){var r,p=arguments;return l()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(7335).then(e.bind(e,37335));case 2:return n.abrupt("return",(r=n.sent).default.apply(r,p));case 3:case"end":return n.stop()}},d)}));function o(){return c.apply(this,arguments)}return o}()}},"src-select-demo-selectconfig":{component:a.memo(a.lazy(function(){return e.e(2433).then(e.bind(e,20273))})),asset:{type:"BLOCK",id:"src-select-demo-selectconfig",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(59014).Z},react:{type:"NPM",value:"18.3.1"},"react-papercss-design":{type:"NPM",value:"0.1.0"}},entry:"index.tsx",title:"\u53EF\u914D\u7F6E",description:"\u4F7F\u7528options\u8FDB\u884C\u914D\u7F6E\u5316"},context:{react:s||(s=e.t(a,2)),"react-papercss-design":v},renderOpts:{compile:function(){var c=i()(l()().mark(function d(){var r,p=arguments;return l()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(7335).then(e.bind(e,37335));case 2:return n.abrupt("return",(r=n.sent).default.apply(r,p));case 3:case"end":return n.stop()}},d)}));function o(){return c.apply(this,arguments)}return o}()}}}},64747:function(u,t,e){e.r(t),e.d(t,{texts:function(){return m}});var s=e(31736);const m=[{value:"\u4E0B\u62C9\u9009\u62E9\u5668",paraId:0,tocIndex:0},{value:"\u53C2\u6570",paraId:1,tocIndex:7},{value:"\u8BF4\u660E",paraId:1,tocIndex:7},{value:"\u7C7B\u578B",paraId:1,tocIndex:7},{value:"\u9ED8\u8BA4\u503C",paraId:1,tocIndex:7},{value:"value",paraId:1,tocIndex:7},{value:"\u53D7\u63A7\u4E0B\u4E0B\u62C9\u6846\u5185\u5BB9",paraId:1,tocIndex:7},{value:"string",paraId:1,tocIndex:7},{value:"-",paraId:1,tocIndex:7},{value:"defaultValue",paraId:1,tocIndex:7},{value:"\u975E\u53D7\u63A7\u4E0B\u4E0B\u62C9\u6846\u5185\u5BB9",paraId:1,tocIndex:7},{value:"string",paraId:1,tocIndex:7},{value:"-",paraId:1,tocIndex:7},{value:"onChange",paraId:1,tocIndex:7},{value:"\u4E0B\u62C9\u6846\u5185\u5BB9\u53D8\u5316\u7684\u56DE\u8C03",paraId:1,tocIndex:7},{value:"(value: string, e: ChangeEvent) => void",paraId:1,tocIndex:7},{value:"-",paraId:1,tocIndex:7},{value:"disabled",paraId:1,tocIndex:7},{value:"\u662F\u5426\u7981\u7528",paraId:1,tocIndex:7},{value:"boolean",paraId:1,tocIndex:7},{value:"-",paraId:1,tocIndex:7},{value:"options",paraId:1,tocIndex:7},{value:"\u914D\u7F6E\u5316\u5199\u6CD5",paraId:1,tocIndex:7},{value:"{ label: string, value: string }[]	",paraId:1,tocIndex:7},{value:"-",paraId:1,tocIndex:7},{value:"\u652F\u6301",paraId:2,tocIndex:8},{value:"<option>",paraId:2,tocIndex:8},{value:"\u6807\u7B7E\u6240\u6709\u7684\u5C5E\u6027. \u6CE8\u610F\u5982\u679C\u4F7F\u7528",paraId:2,tocIndex:8},{value:"items",paraId:2,tocIndex:8},{value:"\u8FDB\u884C\u914D\u7F6E\u4F7F\u7528\u7684\u65F6\u5019, \u5185\u5BB9\u4F7F\u7528",paraId:2,tocIndex:8},{value:"label",paraId:2,tocIndex:8},{value:"\u5C5E\u6027",paraId:2,tocIndex:8}]},39887:function(u,t){t.Z=`import React from 'react';
+import { Select } from 'react-papercss-design';
+
+const App: React.FC = () => {
+  return (
+    <Select
+      onChange={(v, e) => {
+        console.log('select onchange value and event:', v, e);
+      }}
+    >
+      <Select.Option value="1">Option 1</Select.Option>
+      <Select.Option value="2">Option 2</Select.Option>
+      <Select.Option value="3">Option 3</Select.Option>
+    </Select>
+  );
+};
+
+export default App;
+`},59014:function(u,t){t.Z=`import React from 'react';
+import { Select } from 'react-papercss-design';
+
+const App: React.FC = () => {
+  return (
+    <Select
+      defaultValue="1"
+      options={[
+        { label: 'Option 1', value: '1' },
+        { label: 'Option 2', value: '2' },
+        { label: 'Option 3', value: '3' },
+      ]}
+    />
+  );
+};
+
+export default App;
+`},89844:function(u,t){t.Z=`import React, { useState } from 'react';
+import { Select } from 'react-papercss-design';
+
+const App: React.FC = () => {
+  const [value, setValue] = useState<string>('1');
+
+  return (
+    <Select
+      value={value}
+      onChange={(v) => {
+        setValue(v);
+      }}
+    >
+      <Select.Option value="1">Option 1</Select.Option>
+      <Select.Option value="2">Option 2</Select.Option>
+      <Select.Option value="3">Option 3</Select.Option>
+    </Select>
+  );
+};
+
+export default App;
+`},91157:function(u,t){t.Z=`import React from 'react';
+import { Select } from 'react-papercss-design';
+
+const App: React.FC = () => {
+  return (
+    <Select disabled>
+      <Select.Option value="1">Option 1</Select.Option>
+      <Select.Option value="2">Option 2</Select.Option>
+      <Select.Option value="3">Option 3</Select.Option>
+    </Select>
+  );
+};
+
+export default App;
+`}}]);
